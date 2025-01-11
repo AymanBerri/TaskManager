4e771ca02386d3a85deb1258244c9bd98d06e8e1
unknown <berriayman11@gmail.com>
2025-01-11 23:19:38 +0300

    Implemented CRUD operations for TaskItem

---
+++ TaskManager/Controllers/TasksController.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections.Generic;
+using System.Linq; // For querying
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Models;
+
+namespace TaskManager.Controllers
+{
+
+    // endpoint reached at:
+    [Route("api/[controller]")]
+    [ApiController]
+
+
+    public class TasksController: ControllerBase
+    {
+
+        private readonly TaskContext _context; // a connection to the DB
+
+        public TasksController(TaskContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TaskItem>>> GetTasks(){
+            return await _context.Tasks.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TaskItem>> GetTaskItem(int id){
+            var taskItem = await _context.Tasks.FindAsync(id);
+
+            if (taskItem == null)
+            {
+                return NotFound();
+            }
+
+            return taskItem;
+        }
+
+        [HttpPost] // create a new record
+        public async Task<ActionResult<TaskItem>> PostTaskItem(TaskItem taskItem){
+            _context.Tasks.Add(taskItem); //adding
+            await _context.SaveChangesAsync(); //waiting for changes "making it official"
+
+            // The response returns a 201 Created status, a link to the task, and the task data itself.
+            return CreatedAtAction(nameof(GetTaskItem), new { id = taskItem.Id }, taskItem);
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTaskItem(int id, TaskItem taskItem){
+            if (id != taskItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(taskItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Tasks.Any(e => e.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTaskItem(int id){
+            var taskItem = await _context.Tasks.FindAsync(id);
+            if (taskItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tasks.Remove(taskItem);
+            await _context.SaveChangesAsync(); // 
+
+            return NoContent(); // returning nothing. 
+        }
+
+    }
+}
--- TaskManager/obj/Debug/net9.0/TaskManager.AssemblyInfo.cs
+++ TaskManager/obj/Debug/net9.0/TaskManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20cd04dfc33d4e9434e2246a9caa69891b4a5d9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+875e0497915c3d101718fa108e1ebadb1cdca5d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
